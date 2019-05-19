12c06d379a7e4347411ecdea9357c8078a2d71cc
Vlad Hybalo <2pi15b.gybalo@gmail.com>
2019-05-19 20:37:31 +0300

    add styles for base button

--- src/components/BaseComponents/BaseButton/index.vue
+++ src/components/BaseComponents/BaseButton/index.vue
@@ -5,15 +5,27 @@
 </template>
 
 <script>
-    export default {
-
-    }
+    export default {}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     button {
+        padding: .5rem 2rem;
+        font-size: 1.2rem;
+        color: #fff;
+        background-color: #007bff;
+        border-color: #007bff;
+        -webkit-border-radius: 1rem;
+        -moz-border-radius: 1rem;
+        border-radius: 1rem;
         outline: none;
-        border: transparent;
+        cursor: pointer;
+        margin-top: 1rem;
+
+        &:hover {
+            background-color: #0069d9;
+            border-color: #0062cc;
+        }
 
     }
 </style>
