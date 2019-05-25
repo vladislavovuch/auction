fee56b00e4847441c32d0dbc732e660f3b9083cf
Vlad Hybalo <2pi15b.gybalo@gmail.com>
2019-05-25 23:14:54 +0300

    add method to generate uuid

--- src/main.js
+++ src/main.js
@@ -25,6 +25,19 @@ Vue.prototype.$closest = function (target, selector, currentTarget = null) {
     return null;
 };
 
+Vue.prototype.$uuid = function () {
+    let d = new Date().getTime();
+    if (window.performance && typeof window.performance.now === "function") {
+        d += performance.now(); //use high-precision timer if available
+    }
+    const guid = 'xxxxxxxx-xxxx-vxxx-hxxx-xxxxxxxxxxxx'.replace(/x/g, function (c) {
+        const r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return guid;
+}
+
 const vm = new Vue({
     router,
     store,
